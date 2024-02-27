7c6383dca78e5e0ed3aab3e683c6e2ce494bdbf5
Jenifer Plata <159405318+Alix-Plata@users.noreply.github.com>
2024-02-27 15:05:31 +0000

    Actualizacion 27 de febrero

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -5,6 +5,19 @@ package miPrincipal;
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
+        System.out.println("Hola soy Jenifer");
+    /* Demostracion de tipo de variable
+     * los tipos de datos float y double y tiene 3 valores especiales:
+     * infinito positivo
+     * infinito negativo
+     * NaN ( Not a Num)
+     */
+    System.out.Principal(Mhat.sqrt(8));// retorna un punto flotante
+    System.out.println(Mhat.sqrt(-1));//es un NaN
+    System.out.println(1.1e5 * 1.1e2);
+    System.out.println (1.10000 * 1.10);
+    System.out.println(1.1e200 * 1.1e200); // esto retorna a un infinoto pos
+    System.out.println(-1.1e200 * 1.1e200); //esto retorna a un infinito negat
+
+}
 }
