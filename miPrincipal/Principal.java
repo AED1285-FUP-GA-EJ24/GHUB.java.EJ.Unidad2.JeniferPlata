267d3408e1d46891e8d200b0442442c067919fe9
Jenifer Plata <159405318+Alix-Plata@users.noreply.github.com>
2024-02-28 15:05:02 +0000

    avance 28 de febrero

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -12,12 +12,52 @@ public class Principal {
      * infinito negativo
      * NaN ( Not a Num)
      */
-    System.out.Principal(Mhat.sqrt(8));// retorna un punto flotante
-    System.out.println(Mhat.sqrt(-1));//es un NaN
+    System.out.println(Math.sqrt(8));// retorna un punto flotante
+    System.out.println(Math.sqrt(-1));//es un NaN
     System.out.println(1.1e5 * 1.1e2);
     System.out.println (1.10000 * 1.10);
     System.out.println(1.1e200 * 1.1e200); // esto retorna a un infinoto pos
     System.out.println(-1.1e200 * 1.1e200); //esto retorna a un infinito negat
+//Tipo de dato char
+char car='A'; //las constantes literales char se coloca entre comillas
+System.out.println(car);
+car = '/165';
+System.out.println(car);
+car = '/u0041',
+System.out.println(car);
+car = '/165'; 
+System.out.println(car);
+//tipo de dato logico
+boolean bandera; // declarandp la variable bolean
+bandera = true;
+bandera = false;
+if (bandera) System.out.println(x:"Es verdadero");
+// ejemplo de envoltorios
+//convertir la cadena "123"a un tipo de dato entero
+System.out.println(Interger.parseInt(s:"123")); 
+System.out.println(Interger.parseInt(s:"165"));
+System.out.println(Interger.parseInt(s:"21"));
+System.out.println(Interger.parseInt(s:"45"));
+System.out.println(Interger.parseInt(s:"45.89"));
+System.out.println(Interger.parseInt(s:"1.5e8"));
+             //LITERALES Y CONSTANTES
+ 
+ //para declararconstante ,anteponemos la palabra final en la declaracion
+ //y se utiliza un identificador mayuscula
 
+     final double PI =3.141592653586793;
+     int octal = 0.15; // la constante 15 esta representada en base octal
+
+     int decimal = 12;// la constante 12 esta representada en base decimal
+
+     int hexadecimal = 0xAB;// la constante A esta presentada base 18
+     // cuando se imprime se convierte en 171 por que AB en hexadecimal 
+     //corresponde a 171 en decimal
+     System.out.println(hexadecimal);
+
+{
+    
 }
+
+  }
 }
