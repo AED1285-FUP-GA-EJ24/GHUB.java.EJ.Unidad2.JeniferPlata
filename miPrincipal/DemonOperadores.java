f47b08d19fe7f68f604696bfa60b130cc5c366d3
Jenifer Plata <159405318+Alix-Plata@users.noreply.github.com>
2024-03-04 14:59:51 +0000

    Actualizacion de 04/02/24

---
+++ miPrincipal/DemonOperadores.java
@@ -0,0 +1,31 @@
+
+
+package miPrincipal;
+
+public class DemonOperadores {
+  // operadores Aritmeticos
+  /*
+   * + suma
+   * - resta 
+   * * multiplicacion
+   * % residuo
+   * ++ incremento
+   * -- decremento
+   */
+  int a,b;
+  a =5;
+  b =15;
+  int c = a + b;
+  System.out.println("Valor de c:"+c);
+  c = a - b;
+  system.out.println("Valor de  c:"+c);
+  c = a * b;
+  system.out.println("Valor de  c:"+c);
+  c = a / b;
+  system.out.println("Valor de  c:"+c);
+  c = a % b;
+  system.out.println("Valor de  c:"+c);
+  float x, y;
+  x =5.0F;
+  y =
+}
